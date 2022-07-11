46ca2e8da4f199f4a1e24bd15ce9b8b70046f199
Dmitry Parfenov <parfenovds@protonmail.com>
2022-07-11 14:13:45 +0300

    Altered branch with altered change

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, you!");
-        System.out.println("Altered change in master");
+        System.out.println("Altered change in fix");
     }
 }
