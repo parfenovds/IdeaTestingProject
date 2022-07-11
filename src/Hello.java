652352f205268de96b055c79a4e978f5f137420d
Dmitry Parfenov <parfenovds@protonmail.com>
2022-07-11 14:22:16 +0300

    Another try with terminal git

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, you!");
         System.out.println("Altered change in fix");
+        System.out.println("Another one?");
     }
 }
